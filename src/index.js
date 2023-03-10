f0a398e86a5c6a0292b11d9a83dde4fc702ee37d
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-03-10 20:22:51 +0530

    fghfgh

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') 
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
