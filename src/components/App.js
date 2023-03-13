7e49d5f478ad17bbbb1d2ae3da5234aa06889491
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-03-13 10:47:51 +0530

    asdfsd

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    <button>submit</button>
-    </>
+      <html>Now I can render any React component on any DOM node I want using ReactDOM.render</html>
   );
 }
 
